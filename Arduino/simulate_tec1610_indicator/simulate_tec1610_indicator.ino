a7ffc78e5b4883e5d676629bf293bb8b7ed789d1
sahar <sahar.sadat.karparvarfard@gmail.com>
2021-10-31 17:55:21 +0330

    first completion of transmitted byte

--- Arduino/simulate_tec1610_indicator/simulate_tec1610_indicator.ino
+++ Arduino/simulate_tec1610_indicator/simulate_tec1610_indicator.ino
@@ -1,47 +1,47 @@
-
-
-uint8_t transmitted_data[5] = {0x90, 0x00, 0x00, 0x0F, 0x4B};
-
-bool Motion;
-bool Zero;
-bool SignFlag;
-bool OverRange;
-bool OverCapacity;
-// decimal point location DCP 3bit
-uint32_t weigh = 0XF3450F4B;
-uint32_t sample = 0;
-uint8_t weigh_array[4];
-bool first_bit;
-
-
+bool MotionFlag = 1;
+bool ZeroFlag = 1;
+bool SignFlag = 1;
+bool OverRange = 1;
+bool OverCapacity = 1;
+uint8_t DCPL = 5;
+uint32_t sample ;
+uint8_t weigh_array[5];
+uint32_t weigh = 0X00000F4B;
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
-  Serial.println(weigh, HEX);
+  Serial.println("this is weigh binary:");
+  Serial.println(weigh, BIN);
 
   sample = weigh;
-  
-  for (int i = 0; i < 4; i++) {
+
+  for (int i = 4; i > 0 ; i--) {
     weigh_array[i] = sample & 0x7F ;
     sample >>= 7;
-    Serial.println(weigh_array[i] , BIN);
-    
-    
-    
-    
+    //Serial.println(weigh_array[i] , BIN);
   }
-  
- // for (int i = 0; i < 4 ; i++)
-  //  Serial.println(weigh_array[i] , HEX);
+  if (SignFlag) weigh_array[1] |= 0x08;
+  if (OverCapacity) weigh_array[1] |= 0x10;
+  if (OverRange) weigh_array[1] |= 0x20;
 
+  weigh_array[0] = DCPL;
+  if (ZeroFlag) weigh_array[0] |= 0x08;
+  if (MotionFlag) weigh_array[0] |= 0x10;
+  weigh_array[0] |= 0x80;
 
+  Serial.println("this is bytes:");
+
+  for (int i = 0; i < 5 ; i++) {
+
+    Serial.println(weigh_array[i] , BIN);
+
+  }
+ 
+  Serial.write(weigh_array,40);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-
-
 }
