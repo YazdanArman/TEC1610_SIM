15856f65ab18ab21ce00969afef7c136e4a46177
Yazdan <yazdan.arman@gmail.com>
2021-10-31 17:55:07 +0330

    implement rs485 send data

--- ARM/RS485_serial_sender/RS485_serial_sender.ino
+++ ARM/RS485_serial_sender/RS485_serial_sender.ino
@@ -5,12 +5,18 @@
 
 #define Heart_Beat_LED_Pin PC13
 
-int previousMillis = 0;
-int interval = 100;
+int Heart_Beat_previousMillis = 0;
+int Heart_Beat_interval = 100;
+
+int RS485_send_previousMillis = 0;
+int RS485_send_interval = 1000;
 
 
 void setup()
 {
+  enableDebugPorts();
+  Serial.end();
+
   Serial_debug_port.begin(115200);
   RS485_serial_port.begin(9600);
   pinMode(Heart_Beat_LED_Pin, OUTPUT);
@@ -18,9 +24,30 @@ void setup()
 
 void loop()
 {
-  if (millis() - previousMillis > interval)
+  if (millis() - Heart_Beat_previousMillis > Heart_Beat_interval)
   {
-    previousMillis = millis();
+    Heart_Beat_previousMillis = millis();
     digitalWrite(Heart_Beat_LED_Pin, !digitalRead(Heart_Beat_LED_Pin));
   }
+
+  if (millis() - RS485_send_previousMillis > RS485_send_interval)
+  {
+    RS485_send_previousMillis = millis();
+    //Serial_debug_port.println(millis());
+    preTransmission();
+    RS485_serial_port.println(millis());
+    postTransmission();
+  }
+}
+
+
+
+void preTransmission()
+{
+  digitalWrite(RS_Enable_Pin, HIGH);
+}
+
+void postTransmission()
+{
+  digitalWrite(RS_Enable_Pin, LOW);
 }
