076f1a7fe39083399634471eb727d88c747fdad9
sahar <sahar.sadat.karparvarfard@gmail.com>
2021-11-02 22:33:42 +0330

    changing variable names and unsuccessful counting float digit number
    why float changes when divided into ten

--- Arduino/simulate_tec1610_indicator/simulate_tec1610_indicator.ino
+++ Arduino/simulate_tec1610_indicator/simulate_tec1610_indicator.ino
@@ -1,39 +1,72 @@
-bool MotionFlag = 1;
-bool ZeroFlag = 1;
-bool SignFlag = 1;
-bool OverRange = 1;
-bool OverCapacity = 1;
-uint8_t DCPL = 5;
-uint32_t sample ;
-uint8_t weigh_array[5];
-uint32_t weigh = 0X00000F4B;
+bool motion_flag = 1;
+bool zero_flag = 0;
+bool sign_flag = 0;
+bool over_range = 0;
+bool over_capacity = 0;
+uint8_t Decimal_Point_Location = 0 ;
+uint32_t weigh_co ;
+uint8_t weigh_array[5] = {0} ;
+double weigh = 5865.123 ;
+int count = 0;
 
 /*
-وزن = 1658
-جايگاه مميز = 1
+  وزن = 1658
+  جايگاه مميز = 1
 */
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
-  Serial.println("this is weigh binary:");
-  Serial.println(weigh, BIN);
 
-  sample = weigh;
+  //weigh_to_Integer = weigh;
+
+  if (weigh < 0) {
+    weigh *= -1;
+    sign_flag = 1;
+  }
+  Serial.println(weigh);
+  Serial.println(((int)weigh));
+  Serial.println(weigh != ((int)weigh));
+
+  while (weigh != ((int)weigh))
+  {
+    count++;
+    weigh = weigh * 10;
+  }
+  Decimal_Point_Location = count;
+  Serial.println(weigh);
+  Serial.println(Decimal_Point_Location);
+  weigh_co = weigh;
+
+  if (weigh > 16777215) {
+    Serial.println("over capacity");
+    over_capacity  = 1;
+  }
+
+  if (weigh > 16777215) {
+    Serial.println("over capacity");
+    over_range     = 1;
+  }
+  //Serial.println("this is weigh binary:");
+  Serial.println(weigh);
+  //  Serial.println(weigh, BIN);
+  // Serial.println(weigh, HEX);
+
+
 
   for (int i = 4; i > 0 ; i--) {
-    weigh_array[i] = sample & 0x7F ;
-    sample >>= 7;
+    weigh_array[i] = weigh_co & 0x7F ;
+    weigh_co >>= 7;
     //Serial.println(weigh_array[i] , BIN);
   }
-  if (SignFlag) weigh_array[1] |= 0x08;
-  if (OverCapacity) weigh_array[1] |= 0x10;
-  if (OverRange) weigh_array[1] |= 0x20;
+  if (sign_flag)      weigh_array[1] |= 0x08;
+  if (over_capacity)  weigh_array[1] |= 0x10;
+  if (over_range)     weigh_array[1] |= 0x20;
 
-  weigh_array[0] = DCPL;
-  if (ZeroFlag) weigh_array[0] |= 0x08;
-  if (MotionFlag) weigh_array[0] |= 0x10;
+  weigh_array[0] = Decimal_Point_Location;
+  if (weigh == 0) weigh_array[0] |= 0x08;
+  if (motion_flag) weigh_array[0] |= 0x10;
   weigh_array[0] |= 0x80;
 
   Serial.println("this is bytes:");
@@ -43,8 +76,8 @@ void setup() {
     Serial.println(weigh_array[i] , BIN);
 
   }
- 
-  Serial.write(weigh_array,40);
+
+  Serial.write(weigh_array, 5);
 }
 
 void loop() {
