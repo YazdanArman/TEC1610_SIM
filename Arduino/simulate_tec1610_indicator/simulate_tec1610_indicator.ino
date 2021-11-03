b9e706e6dc36971d13ac4f684e7a2741b3953d8c
sahar <sahar.sadat.karparvarfard@gmail.com>
2021-11-03 09:56:16 +0330

    delete analyzing number of mantissa(dcpl)
    input the decimal point location manually

--- Arduino/simulate_tec1610_indicator/simulate_tec1610_indicator.ino
+++ Arduino/simulate_tec1610_indicator/simulate_tec1610_indicator.ino
@@ -1,70 +1,50 @@
-bool motion_flag = 1;
+bool motion_flag = 0;
 bool zero_flag = 0;
 bool sign_flag = 0;
 bool over_range = 0;
 bool over_capacity = 0;
-uint8_t Decimal_Point_Location = 0 ;
+uint8_t decimal_point_location = 0 ;
 uint32_t weigh_co ;
 uint8_t weigh_array[5] = {0} ;
-double weigh = 5865.123 ;
-int count = 0;
+int32_t weigh = -1995 ;
+
 
 /*
   وزن = 1658
   جايگاه مميز = 1
 */
-
+// decimal float point number
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
 
-  //weigh_to_Integer = weigh;
+
 
   if (weigh < 0) {
     weigh *= -1;
     sign_flag = 1;
   }
-  Serial.println(weigh);
-  Serial.println(((int)weigh));
-  Serial.println(weigh != ((int)weigh));
 
-  while (weigh != ((int)weigh))
-  {
-    count++;
-    weigh = weigh * 10;
-  }
-  Decimal_Point_Location = count;
-  Serial.println(weigh);
-  Serial.println(Decimal_Point_Location);
   weigh_co = weigh;
-
-  if (weigh > 16777215) {
-    Serial.println("over capacity");
-    over_capacity  = 1;
-  }
-
-  if (weigh > 16777215) {
-    Serial.println("over capacity");
-    over_range     = 1;
-  }
-  //Serial.println("this is weigh binary:");
+  
+  Serial.println("this is weigh binary:");
   Serial.println(weigh);
-  //  Serial.println(weigh, BIN);
-  // Serial.println(weigh, HEX);
+  Serial.println(weigh, BIN);
+  Serial.println(weigh, HEX);
 
 
 
   for (int i = 4; i > 0 ; i--) {
     weigh_array[i] = weigh_co & 0x7F ;
     weigh_co >>= 7;
-    //Serial.println(weigh_array[i] , BIN);
-  }
+     }
+     
   if (sign_flag)      weigh_array[1] |= 0x08;
   if (over_capacity)  weigh_array[1] |= 0x10;
   if (over_range)     weigh_array[1] |= 0x20;
 
-  weigh_array[0] = Decimal_Point_Location;
+  weigh_array[0] = decimal_point_location;
   if (weigh == 0) weigh_array[0] |= 0x08;
   if (motion_flag) weigh_array[0] |= 0x10;
   weigh_array[0] |= 0x80;
@@ -73,11 +53,11 @@ void setup() {
 
   for (int i = 0; i < 5 ; i++) {
 
-    Serial.println(weigh_array[i] , BIN);
+    Serial.println(weigh_array[i] , HEX);
 
   }
 
-  Serial.write(weigh_array, 5);
+  //Serial.write(weigh_array, 5);
 }
 
 void loop() {
